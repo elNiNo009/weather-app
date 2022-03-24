43f5b58aebcc2595f37c47a459eb23e8c19664b3
sarthak chauhan <sarthak.chauhan496@gmail.com>
2022-03-24 15:49:01 +0530

    current temp

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const request=require('request')
 
-const url='http://api.weatherstack.com/current?access_key=24d42d30033dfe0e9d3cca492e9b6af3&query=New%20York'
+const url='http://api.weatherstack.com/current?access_key=24d42d30033dfe0e9d3cca492e9b6af3&query=30.3165,78.0322&units=m'
 
-request({url:url},
+request({url:url,json:true},
     (error,response)=>
     {
         //console.log(response)
-        const data=JSON.parse(response.body)
-        console.log(data.current)
+       // const data=JSON.parse()
+        console.log(response.body.current)
+        console.log("temperature outside= "+ response.body.current.temperature)
         
-    }
-)
+    }) 
